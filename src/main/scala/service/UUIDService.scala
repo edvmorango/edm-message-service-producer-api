c1a5b050d164bb9e3e064cebed80208ff9b7587d
J. Eduardo V. Morango <edvmorango@gmail.com>
2019-05-04 16:51:40 -0300

    feature: uuid service

--- src/main/scala/service/UUIDService.scala
+++ src/main/scala/service/UUIDService.scala
@@ -1,3 +1,27 @@
 package service
 
-class UUIDService {}
+import io.chrisdavenport.fuuid.FUUID
+import scalaz.zio.{Task, TaskR}
+import scalaz.zio.interop.catz._
+
+trait UUID {
+
+  def uuidService: UUID.Service
+
+}
+
+object UUID {
+
+  trait Service {
+
+    def genUUID(): TaskR[Any, String]
+
+  }
+
+}
+
+object UUIDService extends UUID.Service {
+
+  def genUUID(): TaskR[Any, String] = FUUID.randomFUUID[Task].map(_.toString())
+
+}
